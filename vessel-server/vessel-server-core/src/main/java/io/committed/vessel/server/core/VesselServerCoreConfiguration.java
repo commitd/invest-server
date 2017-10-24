b393a2ffddd4dbb10bbf352e3aa4da0233cdbeb3
Chris Flatley <chris@committed.io>
2017-10-24 22:19:55 +0100

    Add random functions to vessel-server-core

---
+++ vessel-server/vessel-server-core/src/main/java/io/committed/vessel/server/core/VesselServerCoreConfiguration.java
@@ -0,0 +1,10 @@
+package io.committed.vessel.server.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = VesselServerCoreConfiguration.class)
+public class VesselServerCoreConfiguration {
+
+}
