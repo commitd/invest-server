14be93c055d96a0f4a005f0d5a12f5335ec6c679
Chris Flatley <chris@committed.io>
2017-10-25 16:38:03 +0200

    Use method security to control access

--- vessel-plugins/vessel-plugin-server-auth/src/main/java/io/committed/vessel/plugin/server/auth/AbstractWithAuthSecurityConfig.java
+++ vessel-plugins/vessel-plugin-server-auth/src/main/java/io/committed/vessel/plugin/server/auth/AbstractWithAuthSecurityConfig.java
@@ -1,11 +1,23 @@
 package io.committed.vessel.plugin.server.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.HttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public abstract class AbstractWithAuthSecurityConfig {
 
+  @Bean
+  public SecurityWebFilterChain springWebFilterChain(final HttpSecurity http) throws Exception {
+    return http
+        // we rely on method security
+        .authorizeExchange()
+        .anyExchange().permitAll()
+        .and()
+        .build();
+  }
 }
