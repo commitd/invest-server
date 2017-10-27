981d55d65dd6d511d3af72264ca8cdb136641f0f
Chris Flatley <chris@committed.io>
2017-10-27 13:48:03 +0200

    Correct jpa pom file after rename

--- vessel-plugins/vessel-plugin-data-jpa/src/main/java/io/committed/vessel/plugins/data/jpa/JpaConfiguration.java
+++ vessel-plugins/vessel-plugin-data-jpa/src/main/java/io/committed/vessel/plugins/data/jpa/JpaConfiguration.java
@@ -1,6 +1,7 @@
 package io.committed.vessel.plugins.data.jpa;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories
