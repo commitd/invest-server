2bf97b2593f700587e9911ff86d65a9f554a0e44
Chris Flatley <chris@committed.io>
2017-10-31 21:21:17 +0100

    Add guava to lib pom

--- vessel-server/vessel-server-app/src/main/java/io/committed/vessel/server/app/config/WebFluxConfig.java
+++ vessel-server/vessel-server-app/src/main/java/io/committed/vessel/server/app/config/WebFluxConfig.java
@@ -2,10 +2,11 @@ package io.committed.vessel.server.app.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
-public class WebFluxConfig {
+public class WebFluxConfig implements WebFluxConfigurer {
 
 
 }
