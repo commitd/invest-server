434ed0e8d2dee4072513922d84797eb20f68f331
Chris Flatley <chris@committed.io>
2017-11-14 16:18:02 +0000

    Add server data project back to the app

--- vessel-server/vessel-server-data/src/main/java/io/committed/vessel/server/data/dataset/DataProviderSpecification.java
+++ vessel-server/vessel-server-data/src/main/java/io/committed/vessel/server/data/dataset/DataProviderSpecification.java
@@ -3,11 +3,15 @@ package io.committed.vessel.server.data.dataset;
 import java.util.Collections;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DataProviderSpecification {
 
   // the dataproviderfactory id
