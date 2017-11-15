d04d13d70140c95d02ad29c56059f0c57d3dafe4
Chris Flatley <chris@committed.io>
2017-11-15 22:05:25 +0000

    Convert creationservice from a bean factory as this was cause over eager initialisation
    Fixes #13

--- vessel-server/vessel-server-data/src/main/java/io/committed/vessel/server/data/VesselServerDataPlugin.java
+++ vessel-server/vessel-server-data/src/main/java/io/committed/vessel/server/data/VesselServerDataPlugin.java
@@ -1,21 +1,9 @@
 package io.committed.vessel.server.data;
 
-import java.util.Collections;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import io.committed.vessel.extensions.VesselServiceExtension;
-import io.committed.vessel.server.data.dataset.Dataset;
-import io.committed.vessel.server.data.providers.DataProvider;
-import io.committed.vessel.server.data.providers.DataProviderFactory;
-import io.committed.vessel.server.data.services.DataProviderFactoryRegistry;
-import io.committed.vessel.server.data.services.DatasetDataProviderCreationService;
-import io.committed.vessel.server.data.services.DatasetProviders;
-import io.committed.vessel.server.data.services.DatasetRegistry;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -25,38 +13,4 @@ public class VesselServerDataPlugin implements VesselServiceExtension {
 
 
 
-  @Bean
-  public DatasetProviders corpusProviders(
-      @Autowired(required = false) final List<DataProvider> providers) {
-    return new DatasetProviders(toSafeList(providers, "data providers"));
-  }
-
-  @Bean
-  public DatasetRegistry datasetRegistry(
-      @Autowired(required = false) final List<Dataset> datasets) {
-    return new DatasetRegistry(toSafeList(datasets, "datasets"));
-  }
-
-  @Bean
-  public DataProviderFactoryRegistry dataProviderFactoryRegistry(
-      @Autowired(required = false) final List<DataProviderFactory<?>> factories) {
-    return new DataProviderFactoryRegistry(toSafeList(factories, "data provider factories"));
-  }
-
-  @Bean
-  public DatasetDataProviderCreationService corpusDataProviderCreationService(
-      final DatasetRegistry datasetRegistry,
-      final DataProviderFactoryRegistry dataProviderFactoryRegistry) {
-    return new DatasetDataProviderCreationService(datasetRegistry, dataProviderFactoryRegistry);
-  }
-
-  private <T> List<T> toSafeList(final List<T> providers, final String name) {
-    final List<T> list = providers == null ? Collections.emptyList() : providers;
-    if (list.isEmpty()) {
-      log.warn("No {} available, no data will be served", name);
-    } else {
-      log.warn("{} {} available", providers.size(), name);
-    }
-    return list;
-  }
 }
