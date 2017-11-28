6c1f53a1bafd4170d100b00694c152281981b7c1
Chris Flatley <chris@committed.io>
2017-11-28 10:05:41 +0000

    Use data hints option for queries to specific the datasource/database they want

--- vessel-server/vessel-server-data/src/main/java/io/committed/vessel/server/data/services/DatasetProviders.java
+++ vessel-server/vessel-server-data/src/main/java/io/committed/vessel/server/data/services/DatasetProviders.java
@@ -10,11 +10,12 @@ import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.Multimaps;
 
 import io.committed.vessel.server.data.providers.DataProvider;
+import io.committed.vessel.server.data.query.DataHints;
 import reactor.core.publisher.Flux;
 
 public class DatasetProviders {
 
-  public static final List<DataProvider> EMPTY_PROVIDER_LIST = Collections.emptyList();
+  private static final List<DataProvider> EMPTY_PROVIDER_LIST = Collections.emptyList();
   private final ImmutableListMultimap<String, DataProvider> providers;
 
   @Autowired
@@ -22,23 +23,42 @@ public class DatasetProviders {
     this.providers = Multimaps.index(providers, DataProvider::getDataset);
   }
 
-  public Flux<DataProvider> findForDataset(final String datasetId) {
+  private List<DataProvider> findAllForDataset(final String datasetId) {
     final ImmutableList<DataProvider> list = providers.get(datasetId);
     if (list == null || list.isEmpty()) {
-      return Flux.empty();
+      return EMPTY_PROVIDER_LIST;
     } else {
-      return Flux.fromIterable(list);
+      return list;
     }
+  }
+
+  public Flux<DataProvider> findForDataset(final String datasetId) {
+    return findForDataset(datasetId, (DataHints) null);
+  }
+
+  public Flux<DataProvider> findForDataset(final String datasetId, final DataHints hints) {
+    final List<DataProvider> datasetProviders = findAllForDataset(datasetId);
+    final DataHints dh = getHints(hints);
+
+    return dh.apply(datasetProviders);
+  }
+
+  public <T> Flux<T> findForDataset(final String datasetId,
+      final Class<T> providerClass) {
+    return findForDataset(datasetId, providerClass, null);
   }
 
   // It is checked...
   @SuppressWarnings("unchecked")
   public <T> Flux<T> findForDataset(final String datasetId,
-      final Class<T> providerClass) {
-    return (Flux<T>) findForDataset(datasetId)
-        .filter(providerClass::isInstance);
+      final Class<T> providerClass, final DataHints hints) {
 
+    return (Flux<T>) findForDataset(datasetId, hints)
+        .filter(providerClass::isInstance);
   }
 
+  private DataHints getHints(final DataHints hints) {
+    return hints != null ? hints : DataHints.DEFAULT;
+  }
 }
 
