8479fcffe9c46c54bb8e154243d822505470c89f
Chris Flatley <chris@committed.io>
2018-01-30 12:02:09 +0000

    Allow configuration of html files for app

--- invest-plugins/invest-plugin-ui-app/src/main/java/io/committed/invest/plugins/ui/application/ApplicationConfig.java
+++ invest-plugins/invest-plugin-ui-app/src/main/java/io/committed/invest/plugins/ui/application/ApplicationConfig.java
@@ -1,10 +1,14 @@
 package io.committed.invest.plugins.ui.application;
 
+import java.io.File;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,11 +17,22 @@ import org.springframework.web.server.WebFilter;
 @Configuration
 public class ApplicationConfig {
 
+  @Autowired
+  private UiApplicationSettings settings;
+
   @Bean
   @Order(value = Ordered.LOWEST_PRECEDENCE)
   public RouterFunction<ServerResponse> applicationRootRoutes() {
+    Resource resource;
+    if (settings.isHostedFromFileSystem()) {
+      resource = new PathResource(new File(settings.getDirectory()).toPath());
+    } else {
+      // Fallback to classpath
+      resource = new ClassPathResource("/ui/app/", this.getClass().getClassLoader());
+    }
+
     return RouterFunctions.resources("/ui/app/**",
-        new ClassPathResource("/ui/app/", this.getClass().getClassLoader()));
+        resource);
   }
 
   @Bean
--- invest-plugins/invest-plugin-ui-app/src/main/java/io/committed/invest/plugins/ui/application/UiApplicationSettings.java
+++ invest-plugins/invest-plugin-ui-app/src/main/java/io/committed/invest/plugins/ui/application/UiApplicationSettings.java
@@ -4,9 +4,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import lombok.Data;
 
 @Data
-@ConfigurationProperties("vessel.app")
+@ConfigurationProperties("invest.app")
 public class UiApplicationSettings {
 
   private String title = "Vessel";
 
+  private String directory = null;
+
+
+  public boolean isHostedFromFileSystem() {
+    return directory != null;
+  }
 }
