da1399068a719c52fb053daa596276189f7e0ba7
Chris Flatley <chris@committed.io>
2018-01-30 12:01:06 +0000

    ExtensionLogger prints outs multi extensions

--- invest-server/invest-server-core/src/main/java/io/committed/invest/server/core/services/ExtensionLogger.java
+++ invest-server/invest-server-core/src/main/java/io/committed/invest/server/core/services/ExtensionLogger.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import io.committed.invest.extensions.InvestExtension;
+import io.committed.invest.extensions.collections.InvestExtensions;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -14,12 +15,31 @@ public class ExtensionLogger {
   @Autowired(required = false)
   private List<InvestExtension> extensions;
 
+  @Autowired(required = false)
+  private List<InvestExtensions> multiExtensions;
+
   @PostConstruct
   public void postConstruct() {
-    if (extensions == null || extensions.isEmpty()) {
-      log.warn("No extensions");
+    int total = 0;
+
+    if (extensions != null && !extensions.isEmpty()) {
+      extensions.forEach(this::logExtension);
+      total += extensions.size();
+    }
+
+    if (multiExtensions != null && !multiExtensions.isEmpty()) {
+      multiExtensions.forEach(m -> {
+        m.stream().forEach(this::logExtension);
+      });
+
+      total += multiExtensions.stream().reduce(0, (a, e) -> a + e.getExtensions().size(), Integer::sum);
+    }
+
+    if (total == 0) {
+      log.warn("No extensions found");
     } else {
-      extensions.stream().forEach(this::logExtension);
+      log.info("Found {} extensions", total);
+
     }
   }
 
