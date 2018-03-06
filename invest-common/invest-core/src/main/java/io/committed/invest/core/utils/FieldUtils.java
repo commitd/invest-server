d83b29185a5eea27ea9cf058e42fd41b4c5aaad3
Chris Flatley <chris@committed.io>
2018-03-06 11:44:16 +0000

    Add join fields from string[]

--- invest-common/invest-core/src/main/java/io/committed/invest/core/utils/FieldUtils.java
+++ invest-common/invest-core/src/main/java/io/committed/invest/core/utils/FieldUtils.java
@@ -22,4 +22,8 @@ public class FieldUtils {
     return FIELD_SPLITTER.splitToList(field);
   }
 
+  public static String joinField(final String... path) {
+    return FIELD_JOINER.join(path);
+  }
+
 }
