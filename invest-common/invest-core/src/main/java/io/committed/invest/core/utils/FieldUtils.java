dc5c73a9e2274cb31a8725d35623fc98141bcdeb
Chris Flatley <chris@committed.io>
2018-03-06 10:44:56 +0000

    Move fieldutils to core

---
+++ invest-common/invest-core/src/main/java/io/committed/invest/core/utils/FieldUtils.java
@@ -0,0 +1,25 @@
+package io.committed.invest.core.utils;
+
+import java.util.Collections;
+import java.util.List;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+
+public class FieldUtils {
+  private static final Joiner FIELD_JOINER = Joiner.on(".").skipNulls();
+
+  // Fixed limit here to avoid some crazy depth, when in reality it'll be 2.
+  private static final Splitter FIELD_SPLITTER = Splitter.on(".").trimResults().omitEmptyStrings().limit(5);
+
+  public static String joinField(final List<String> path) {
+    return FIELD_JOINER.join(path);
+  }
+
+  public static List<String> fieldSplitter(final String field) {
+    if (field == null) {
+      return Collections.emptyList();
+    }
+    return FIELD_SPLITTER.splitToList(field);
+  }
+
+}
