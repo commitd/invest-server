7b376f428353cc679052fbcaadc08da2af9952cf
Chris Flatley <chris@committed.io>
2018-03-16 18:52:22 +0000

    Fix broken test

--- invest-plugins/invest-plugin-ui-host/src/test/java/io/committed/invest/plugins/ui/host/LombokTest.java
+++ invest-plugins/invest-plugin-ui-host/src/test/java/io/committed/invest/plugins/ui/host/LombokTest.java
@@ -1,14 +1,19 @@
 package io.committed.invest.plugins.ui.host;
 
+import static org.mockito.Mockito.mock;
 import org.junit.Test;
+import org.springframework.core.io.Resource;
 import io.committed.invest.plugins.ui.host.data.PluginJson;
 import io.committed.invest.plugins.ui.host.data.PluginOverride;
+import io.committed.invest.test.BeanTestSupport;
 import io.committed.invest.test.LombokDataTestSupport;
 
 public class LombokTest {
 
   @Test
   public void testLombok() {
+    BeanTestSupport.addFactory(Resource.class, () -> mock(Resource.class));
+
     final LombokDataTestSupport mt = new LombokDataTestSupport();
     mt.testClass(UiHostSettings.class);
     mt.testClasses(PluginJson.class, PluginOverride.class);
