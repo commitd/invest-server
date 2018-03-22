c4ecbf5032474b0638441b7444f3f1982e3b46a7
Chris Flatley <chris@committed.io>
2018-03-22 20:38:59 +0000

    Add check for ui-app

---
+++ invest-plugins/invest-plugin-ui-app/src/test/java/io/committed/invest/plugins/ui/application/InvestUiApplicationExtensionTest.java
@@ -0,0 +1,39 @@
+package io.committed.invest.plugins.ui.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import io.committed.invest.test.InvestTestContext;
+
+
+@RunWith(SpringRunner.class)
+@WebFluxTest
+@ContextConfiguration(classes = {InvestTestContext.class, InvestUiApplicationExtension.class})
+@DirtiesContext
+public class InvestUiApplicationExtensionTest {
+
+  @Autowired
+  private WebTestClient webClient;
+
+  @Test
+  public void redirectFromRoot() {
+    this.webClient.get()
+        .uri("/")
+        .exchange()
+        .expectStatus().is3xxRedirection();
+  }
+
+  @Test
+  public void getIndex() {
+    this.webClient.get()
+        .uri("/ui/app/index.html")
+        .exchange()
+        .expectStatus().is2xxSuccessful();
+  }
+
+}
