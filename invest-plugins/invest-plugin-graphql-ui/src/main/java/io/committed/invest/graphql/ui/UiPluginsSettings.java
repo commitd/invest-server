2dd7fcd444b925394f42bb9c6d0ec3936a540292
Chris Flatley <chris@committed.io>
2018-03-29 13:38:56 +0100

    Correct settings prefixed vessel not invest

--- invest-plugins/invest-plugin-graphql-ui/src/main/java/io/committed/invest/graphql/ui/UiPluginsSettings.java
+++ invest-plugins/invest-plugin-graphql-ui/src/main/java/io/committed/invest/graphql/ui/UiPluginsSettings.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /** Settings for the UI plugins */
 @Data
-@ConfigurationProperties("vessel.ui")
+@ConfigurationProperties("invest.ui")
 public class UiPluginsSettings {
 
   /**
