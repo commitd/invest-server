f2bb824aecbbb5088831ff6f43652fb3e1a2e44f
Chris Flatley <chris@committed.io>
2018-05-16 18:22:44 +0100

    Bump java dependencies and fix api changes

--- invest-plugins/invest-plugin-graphql/src/main/java/io/committed/invest/server/graphql/mappers/PropertiesListScalarAdapter.java
+++ invest-plugins/invest-plugin-graphql/src/main/java/io/committed/invest/server/graphql/mappers/PropertiesListScalarAdapter.java
@@ -2,16 +2,11 @@ package io.committed.invest.server.graphql.mappers;
 
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Type;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-
-import graphql.schema.GraphQLScalarType;
-
 import io.committed.invest.core.dto.collections.PropertiesList;
 import io.committed.invest.core.dto.collections.Property;
-
+import graphql.schema.GraphQLScalarType;
 import io.leangen.geantyref.GenericTypeReflector;
 import io.leangen.graphql.execution.ResolutionEnvironment;
 import io.leangen.graphql.generator.BuildContext;
@@ -23,44 +18,18 @@ import io.leangen.graphql.util.Scalars;
 /**
  * A dedicated mapper to convert {@link PropertiesList} from/to a scalar.
  *
- * <p>This is necessary because of the Object values in the Properties. Allowing SPQR to 'ignore'
- * the content of the PropertiesList and pass it through verbatim as JSON matches the intend of
+ * <p>
+ * This is necessary because of the Object values in the Properties. Allowing SPQR to 'ignore' the
+ * content of the PropertiesList and pass it through verbatim as JSON matches the intend of
  * PropertiesList/Map which is a catch all for 'stuff' we don't.
  *
- * <p>Based on SPQR's from ObjectScalarMapper
+ * <p>
+ * Based on SPQR's from ObjectScalarAdaptor
  */
 public class PropertiesListScalarAdapter extends CachingMapper<GraphQLScalarType, GraphQLScalarType>
-    implements OutputConverter<PropertiesList, List<Property>> {
+implements OutputConverter<PropertiesList, List<Property>> {
 
   private static final AnnotatedType LIST = GenericTypeReflector.annotate(LinkedList.class);
-
-  @Override
-  public GraphQLScalarType toGraphQLType(
-      final String typeName,
-      final AnnotatedType javaType,
-      final Set<Type> abstractTypes,
-      final OperationMapper operationMapper,
-      final BuildContext buildContext) {
-    buildContext.knownInputTypes.add(typeName);
-    return Scalars.graphQLObjectScalar(typeName);
-  }
-
-  @Override
-  public GraphQLScalarType toGraphQLInputType(
-      final String typeName,
-      final AnnotatedType javaType,
-      final Set<Type> abstractTypes,
-      final OperationMapper operationMapper,
-      final BuildContext buildContext) {
-    buildContext.knownTypes.add(typeName);
-    return toGraphQLType(typeName, javaType, abstractTypes, operationMapper, buildContext);
-  }
-
-  @Override
-  protected void registerAbstract(
-      final AnnotatedType type, final Set<Type> abstractTypes, final BuildContext buildContext) {
-    abstractTypes.addAll(collectAbstract(type, new HashSet<>(), buildContext));
-  }
 
   @Override
   public List<Property> convertOutput(
@@ -74,5 +43,19 @@ public class PropertiesListScalarAdapter extends CachingMapper<GraphQLScalarType
   public boolean supports(final AnnotatedType type) {
     final Type actualType = type.getType();
     return actualType.equals(PropertiesList.class);
+  }
+
+  @Override
+  protected GraphQLScalarType toGraphQLType(final String typeName, final AnnotatedType javaType,
+      final OperationMapper operationMapper, final BuildContext buildContext) {
+    return Scalars.graphQLObjectScalar(typeName);
+  }
+
+  @Override
+  protected GraphQLScalarType toGraphQLInputType(final String typeName,
+      final AnnotatedType javaType,
+      final OperationMapper operationMapper, final BuildContext buildContext) {
+    return toGraphQLType(typeName, javaType, operationMapper, buildContext);
+
   }
 }
